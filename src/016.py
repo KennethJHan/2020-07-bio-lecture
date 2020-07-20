1e2c6f89a67471999344c2763800b495ff1f4b82
KennethJHan <kenneth.jh.han@gmail.com>
2020-07-20 16:35:22 +0900

    day1_

--- src/016.py
+++ src/016.py
@@ -1,5 +1,9 @@
 import sys
 
+if len(sys.argv) != 2:
+    print(f"#usage: python {sys.argv[0]} [fasta]")
+    sys.exit()
+
 f = sys.argv[1]
 d = {}
 
@@ -12,5 +16,14 @@ with open(f, 'r') as handle:
                 d[s] += 1
             else:
                 d[s] = 1
-print(d)
+
+total = 0
+for k,v in d.items():
+    total += v
+
+with open("result.txt", 'w') as handle:
+    handle.write(f"A: {d['A']}\n")
+    handle.write(f"C: {d['C']}\n")
+    handle.write(f"G: {d['G']}\n")
+    handle.write(f"T: {d['T']}\n")
 
